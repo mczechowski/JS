7add4631159c80e40335a182ae13804a67537c2c
mczechowski <m.czechowski@interia.pl>
2019-07-14 22:25:18 +0200

    reading input data

--- Budget app/app.js
+++ Budget app/app.js
@@ -1,6 +1,6 @@
 
 ///////////////// BUDGET CONTROLLER
-const budgetController = (function(){
+const budgetController = (function () {
 
     //some code
 
@@ -10,24 +10,41 @@ const budgetController = (function(){
 
 
 ///////////////// UI CONTROLLER
-const UIController = (function(){
+const UIController = (function () {
 
-    
+    let DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,  //dokonczyc inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    }
 
 })();
 
 
 
-
-
 //////////////// GLOBAL APP CONTROLLER
-const controller = (function(budgetCtrl, UICtrl){
+const controller = (function (budgetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function(){
-        console.log('test click');
+    const DOM = UICtrl.getDOMstrings();
 
+    let ctrlAddItem = function () {
         // 1. Get the field input data
-
+        const input = UICtrl.getInput();
+        console.log(input);
         // 2. Add the item to the budget controller
 
         // 3. Add the item to the UI
@@ -35,7 +52,17 @@ const controller = (function(budgetCtrl, UICtrl){
         // 4. Calculate budget
 
         // 5. Display the buget on the UI
+        console.log('It works!')
+    }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+            //event.preventDefault(); //prevents the enter key form also triggering a click event
+            ctrlAddItem();
+        }
     });
-    
+
 })(budgetController, UIController);
 
