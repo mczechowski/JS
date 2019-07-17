753b908d33613bb5090a5e5f1f89bfab05703422
Mariusz Czechowski <mczechowski@opidomain.local>
2019-07-17 12:26:11 +0200

    Add a new item to the UI

--- Budget app/app.js
+++ Budget app/app.js
@@ -2,19 +2,19 @@
 ///////////////// BUDGET CONTROLLER ////////////////////////////////////////////////////////
 const budgetController = (function () {
 
-    let Expense = function(id, description, value){
+    let Expense = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
-    let Income = function(id, description, value){
+    let Income = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
-    
+
     let data = {
-        allItemsL: {
+        allItems: {
             exp: [],
             inc: []
         },
@@ -24,10 +24,35 @@ const budgetController = (function () {
         }
     }
 
+    return {
+        addItem: function (type, des, val) {
+            let newItem, ID;
+
+            //Create new ID
+            if (data.allItems[type].length > 0) {
+                //                  exp  [2 -1].id+1
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            //Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            //push it into our data structure
+            data.allItems[type].push(newItem);
+
+            //return the new element
+            return newItem;
+        }
+    };
+
 
 })();
-
-
 
 
 ///////////////// UI CONTROLLER ///////////////////////////////////////////////////////////
@@ -37,7 +62,9 @@ const UIController = (function () {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
 
     return {
@@ -48,6 +75,27 @@ const UIController = (function () {
                 value: document.querySelector(DOMstrings.inputValue).value
             };
         },
+
+        addListItem: function (obj, type) {
+            let html, newHtml, element;
+            //Create HTML string with placeholder txt
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item" id="income-%id%"><div class="item__description">%description%</div><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div>';
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item" id="expense-%id%"><div class="item__description">%description%</div><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div>'
+            }
+
+            //Replace the placeholder txt with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            //Insert HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -76,10 +124,13 @@ const controller = (function (budgetCtrl, UICtrl) {
         // 1. Get the field input data
         const input = UICtrl.getInput();
         console.log(input);
+
         // 2. Add the item to the budget controller
+        //getInput.type, getInput.description ... from UI conroller
+        let newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
-
+        UICtrl.addListItem(newItem, input.type)
         // 4. Calculate budget
 
         // 5. Display the buget on the UI
