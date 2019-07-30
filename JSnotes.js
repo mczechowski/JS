8019b4e6ac6388a93958ef6b8d3e359a85158d03
Mariusz Czechowski <mczechowski@opidomain.local>
2019-07-30 15:07:51 +0200

    add Asynchronous JavaScript notes

--- JSnotes.js
+++ JSnotes.js
@@ -157,18 +157,18 @@
     interviewQuestion('designer')('Mark'); //designer -> function(name)
 
 }
-//================================== 4. ES6 ==================================
-
-//IIFEs in ES6
+//================================== 4. ES6 features ==========================================
 {
-    let a;
-    const b;
-}
+    //IIFEs in ES6
+    {
+        let a;
+        const b;
+    }
 
-//string - backticks - template literal
+    //string - backticks - template literal
     console.log(`This is ${firstName}. He was born in ${yearOfBirth}.`)
 
-//arrow functions =>
+    //arrow functions =>
     const years = [1992, 2000, 1974, 1956];
     ////ES5
     var ages5 = years.map(function (el) {
@@ -198,7 +198,7 @@
     new Person('Mike').myFriends6(friends);
     const friends = ['Bob', 'John', 'Jane'];
 
-//destructuring
+    //destructuring
     ////ES5
     var john = ['John', 25];
     var name = john[0];
@@ -214,7 +214,7 @@
     const { firstName, lastName } = obj
     const { firstName: a, lastName: b } = obj  // variablle firstName is called a
 
-//spread operator
+    //spread operator
     function addFourAges(a, b, c, d) {
         return a + b + c + d;
     }
@@ -228,14 +228,14 @@
     //Array.from(all); 
     all.forEach(cur => cur.style.color = 'purple');
 
-//rest parameters
+    //rest parameters
     function isFullAge(limit, ...years) {
         //console.log(years);
         years.forEach(cur => console.log((2016 - cur) >= limit));
     }
     isFullAge(18, 1990, 1999, 2015);
 
-//default parameters
+    //default parameters
     function SmithPerson(firstName, year, lastName = 'Smith', nationality = 'american') {
         this.firstName = firstName;
         this.year = year;
@@ -244,7 +244,7 @@
     }
     let john = new SmithPerson('John', 1990);
 
-//maps
+    //maps
     const question = new Map();
     question.set('question', 'What is the official name of the lastest major JS version?')
     question.set(1, 'ES5');
@@ -277,7 +277,7 @@
     console.log(question.get(ans === question.get('correct')));
 
 
-//classes
+    //classes
     class Person6 {
         constructor(name, year, job) {
             this.fullName = name;
@@ -298,7 +298,7 @@
     john6.calculateAge();
     Person6.greeting();
 
-//classes with subclasses
+    //classes with subclasses
     ////ES5
     var Person5 = function (name, year, job) {
         this.fullName = name;
@@ -321,6 +321,7 @@
 
     var johnAlglere5 = new Athlete5('John', 1990, 'swimmer', 3, 10);
     johnAlglere5.calculateAge();
+
     ////ES6
     class Person6 {
         constructor(name, year, job) {
@@ -342,6 +343,7 @@
     class Athlete6 extends Person6 {
         constructor(name, yearOfBirth, job, olympicGames, medals) {
             super(name, yearOfBirth, job);
+
             this.olympicGames = olympicGames;
             this.medals = medals;
         }
@@ -354,3 +356,153 @@
     const johnAthlere6 = new Athlete5('John', 1990, 'swimmer', 3, 10);
     johnAlglere5.calculateAge();
     johnAlglere5.wonMedal();
+}
+//================================== 5. Asynchronous JS =======================================
+{
+    // setTimeout
+    const second = () => {
+        setTimeout(() => {
+            console.log('Async hey there!')
+        }, 2000)
+    };
+    const first = () => {
+        console.log('Hey there');
+        second();
+        console.log('The end');
+    };
+
+
+    //async with callbacks - callback hell
+    function getRecipe() {
+        setTimeout(() => {
+            const recipeID = [523, 634, 745, 834];
+            console.log(recipeID);
+
+            setTimeout((id) => {
+                const recipe = { title: 'Fresh tomat pasta', publisher: 'Jonas' };
+                console.log(`${id}: ${recipe.title}`);
+
+                setTimeout(publisher => {
+                    const recipe = { title: 'Pizza', publisher: 'Jonas' };
+                    console.log(recipe);
+                }, 1500, recipe.publisher)
+
+            }, 1000, recipeID[2]);
+
+        }, 1500);
+    };
+    getRecipe();
+
+
+    //promises
+    const getIDs = new Promise((resolve, reject) => {
+        setTimeout(() => {   //simulate api call
+            resolve([523, 634, 745, 834]);
+        }, 1500);
+    });
+
+    const getRecipe = recID => {
+        return new Promise((resolve, reject) => {
+            setTimeout(ID => {
+                const recipe = { title: 'Fresh tomat pasta', publisher: 'Jonas' };
+                resolve(`${id}: ${recipe.title}`);
+            }, 1500, recID);
+        });
+    };
+
+    const getRelated = publisher => {
+        return new Promise((resolve, reject) => {
+            setTimeout(pub => {
+                const recipe = { title: 'Pizza', publisher: 'Jonas' };
+                resolve(`${pub}: ${recipe.title}`);
+            }, 1500, publisher);
+        });
+    };
+
+    getIDs
+        .then(IDs => {
+            console.log(IDs);
+            return getRecipe(IDs[2]);
+        })
+        .then(recipe => {
+            console.log(recipe);
+            return getRelated('Jonas');
+        })
+        .then(recipe => {
+            console.log(recipe);
+        })
+        .catch(error => {
+            console.log('Error!!')
+        });
+
+
+    //async-await
+    async function getRecipesAW() {
+        const IDs = await getIDs;
+        console.log(IDs);
+
+        const recipe = await getRecipe(IDs[2]);
+        console.log(recipe);
+
+        const relared = await getRelated('Jonas');
+        console.log(relared);
+    }
+    getRecipesAW();
+
+
+    //AJAX calls, fetch with promises
+    function getWeather(weatherID) {
+        fetch(`https://api.codetabs.com/v1/proxy?quest=https://www.metaweather.com/api/location/${weatherID}/`)
+            .then(result => {
+                console.log(result);
+                return result.json();
+            })
+            .then(jsondata => {
+                console.log(jsondata);
+                const today = jsondata.consolidated_weather[0];
+                console.log(`Temperature in ${jsondata.title} is: ${today.the_temp} *C`);
+            })
+            .catch(error => {
+                console.log(`We have a problem: ${error}`);
+            });
+    }
+
+    getWeather(2487956);
+    getWeather(523920);
+
+
+    //AJAX with async/await
+    async function getWeatherAW(weatherID) {
+        const result = await fetch(`https://api.codetabs.com/v1/proxy?quest=https://www.metaweather.com/api/location/${weatherID}/`)
+        const jsondata = await result.json();
+        // console.log(jsondata);
+        const tomorrow = jsondata.consolidated_weather[1];
+        console.log(`Tomorrow temperature in ${jsondata.title} is: ${tomorrow.the_temp} *C`);
+        return jsondata;
+    }
+    getWeatherAW(2487956);
+    getWeatherAW(523920);
+
+    
+    let warsaw;
+    getWeatherAW(523920).then(jsondata => {
+        warsaw = jsondata
+        console.log(warsaw);
+    });
+
+
+    //try - catch -error catch
+    async function getWeatherAW(weatherID) {
+        try {
+            const result = await fetch(`https://api.codetabs.com/v1/proxy?quest=https://www.metaweather.com/api/location/${weatherID}/`)
+            const jsondata = await result.json();
+            // console.log(jsondata);
+            const tomorrow = jsondata.consolidated_weather[1];
+            console.log(`Tomorrow temperature in ${jsondata.title} is: ${tomorrow.the_temp} *C`);
+
+        } catch (error) {
+            console.log(`We have a error: ${error}`);
+        }
+    }
+
+}
